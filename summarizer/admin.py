0f408b200182c4f35ca97a63531ed6a70ad372e2
Piotr Rybinski <prybinski@macbookpro.home>
2025-04-11 00:06:07 +0200

    DB redesign

--- summarizer/admin.py
+++ summarizer/admin.py
@@ -1,51 +1,20 @@
 from django.contrib import admin
 from django import forms
-from .models import PromptCategory, PromptInternalRequest, PromptInternalResponse, PromptExternalRequest, PromptExternalResponse
+from .models import PromptRequest, PromptResponse
 from summarizer.templates_generator.connector import Connector
 
-class PromptInternalRequestForm(forms.ModelForm):
+class PromptRequestForm(forms.ModelForm):
 
     class Meta:
-        model = PromptInternalRequest
+        model = PromptRequest
         exclude = ['processed']
-        fields = ('category', 'file', 'prompt', 'created_by')
+        fields = ('file', 'prompt', 'created_by')
 
     def __init__(self, *args, **kwargs):
-        super(PromptInternalRequestForm, self).__init__(*args, **kwargs)
+        super(PromptRequestForm, self).__init__(*args, **kwargs)
         self.fields['file'].required = False
 
-class PromptInternalResponseForm(forms.ModelForm):
-
-    class Meta:
-        model = PromptInternalResponse
-        fields = ('prompt_request', 'response')
-
-    def __init__(self, *args, **kwargs):
-        super(PromptInternalResponseForm, self).__init__(*args, **kwargs)
-        self.fields['prompt_request'].widget.attrs['readonly'] = True
-        self.fields['response'].widget.attrs['readonly'] = True
-
-
-class PromptExternalRequestForm(forms.ModelForm):
-
-    class Meta:
-        model = PromptExternalRequest
-        exclude = ['processed']
-        fields = ('category', 'prompt', 'created_by')
-
-
-class PromptExternalResponseForm(forms.ModelForm):
-
-    class Meta:
-        model = PromptExternalResponse
-        fields = ('prompt_request', 'response')
-
-    def __init__(self, *args, **kwargs):
-        super(PromptExternalResponseForm, self).__init__(*args, **kwargs)
-        self.fields['prompt_request'].widget.attrs['readonly'] = True
-        self.fields['response'].widget.attrs['readonly'] = True
-
-class PromptInternalRequestAdmin(admin.ModelAdmin):
+class PromptRequestAdmin(admin.ModelAdmin):
 
     actions = ['call_ai']
 
@@ -54,9 +23,9 @@ class PromptInternalRequestAdmin(admin.ModelAdmin):
         selected = queryset.values_list('pk', flat=True)
         # @ToDo - improve db performance by moving query out of the loop
         for item in selected:
-            prompt_object = PromptInternalRequest.objects.get(pk=item)
+            prompt_object = PromptRequest.objects.get(pk=item)
             if prompt_object.processed == False:
-                prompt_object = PromptInternalRequest.objects.get(pk=item)
+                prompt_object = PromptRequest.objects.get(pk=item)
                 ai_responsne = Connector.call_gpt(self, prompt_object.prompt)
                 prompt_response = ai_responsne
                 prompt_response.save()
@@ -64,31 +33,7 @@ class PromptInternalRequestAdmin(admin.ModelAdmin):
                 prompt_object.save()
                 
 
-class PromptExternalResponseAdmin(admin.ModelAdmin):
+class PromptResponse(admin.ModelAdmin):
     readonly_fields=('prompt_request', 'response')
 
-class PromptExternalRequestAdmin(admin.ModelAdmin):
-
-    actions = ['call_ai']
-
-    @admin.action(description='Send prompts to AI for selected request')
-    def call_ai(self, request, queryset):
-        selected = queryset.values_list('pk', flat=True)
-        # @ToDo - improve db performance by moving query out of the loop
-        for item in selected:
-            prompt_object = PromptExternalRequest.objects.get(pk=item)
-            if prompt_object.processed == False:
-                prompt_object = PromptExternalRequest.objects.get(pk=item)
-                ai_responsne = Connector.call_gpt(self, prompt_object.prompt)
-                prompt_response = ai_responsne
-                prompt_response.save()
-                prompt_object.processed = True
-                prompt_object.save()
-                
-
-class PromptExternalResponseAdmin(admin.ModelAdmin):
-    readonly_fields=('prompt_request', 'response')
-
-admin.site.register(PromptCategory)
-admin.site.register(PromptInternalRequest, PromptInternalRequestAdmin)
-admin.site.register(PromptExternalResponse, PromptExternalResponseAdmin)
+admin.site.register(PromptRequest, PromptRequestAdmin)
--- summarizer/models.py
+++ summarizer/models.py
@@ -1,68 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class PromptCategory(models.Model):
-    category_name = models.CharField(max_length=200)
-    category_reference = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        db_table = 'prompt_category'
-
-class PromptInternalRequest(models.Model):    
+class PromptRequest(models.Model):    
 
     def user_directory_path(self, instance, filename): 
         # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
         return 'user_{0}/{1}'.format(instance.user.id, filename) 
     
-    category = models.ForeignKey('PromptCategory', on_delete=models.CASCADE)
     prompt = models.TextField()
     file = models.FileField(upload_to = user_directory_path, blank=True, null=True) 
-    accepted_internally = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    processed = models.BooleanField(default=False)
+    internal = models.BooleanField(default=False)
+    external = models.BooleanField(default=False)
+    processed_internally = models.BooleanField(default=False)
+    accepted = models.BooleanField(default=False)
+    processed_externally = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created']
-        db_table = 'prompt_internal_request'
+        db_table = 'prompt_request'
 
-class PromptExternalRequest(models.Model):    
-    
-    category = models.ForeignKey('PromptCategory', on_delete=models.CASCADE)
-    prompt = models.TextField()
-    accepted_internally = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    processed = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-created']
-        db_table = 'prompt_external_request'
-
-    def save_model(self, request, instance, form, change):
-        user = request.user 
-        instance = form.save(commit=False)
-        if not change or not instance.created_by:
-            instance.created_by = user
-        instance.save()
-        form.save_m2m()
-        return instance
-
-class PromptInternalResponse(models.Model):
-    prompt_request = models.ForeignKey('PromptInternalRequest', on_delete=models.CASCADE)
+class PromptResponse(models.Model):
+    prompt_request = models.ForeignKey('PromptRequest', on_delete=models.CASCADE)
     response = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created']
-        db_table = 'prompt_internal_response'
-
-class PromptExternalResponse(models.Model):
-    prompt_request = models.ForeignKey('PromptExternalRequest', on_delete=models.CASCADE)
-    response = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-created']
-        db_table = 'prompt_external_response'
+        db_table = 'prompt_response'
